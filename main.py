1b7e2d037ffbbd7ec91fb7236bc4fe074b2fb314
Jmk0213 <jmk070213@naver.com>
2025-05-30 12:36:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,9 @@ df = pd.read_csv("Delivery.csv")
 st.subheader("데이터 미리보기")
 st.dataframe(df.head())
 
-st.write(df.columns)
-
 # 위도/경도 컬럼명 고정 (데이터 컬럼명에 맞게 변경!)
-lat_col = 'latitude'
-lon_col = 'longitude'
+lat_col = 'Latitude'
+lon_col = 'Longitude'
 
 # 클러스터 개수 선택
 n_clusters = st.slider("클러스터 개수 선택 (K)", min_value=2, max_value=10, value=3)
